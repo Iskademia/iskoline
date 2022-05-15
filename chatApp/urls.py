b11a7fcde9d189e34798ef8573c00d178155de1b
Faith <faithe.marco@gmail.com>
2022-05-16 02:30:12 +0800

    fm- frontend-navbar commit

--- chatApp/urls.py
+++ chatApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Fronpage, name='frontpage'),
-    path('dashboard/', views.Index, name='index'),
+    #path('', views.Fronpage, name='frontpage'),
+    path('', views.Index, name='index'),
     path("chat/<str:room_name>/", views.Room, name='room'),
     path("room", views.EnterRoom, name='enterroom'),
     # Save Room
